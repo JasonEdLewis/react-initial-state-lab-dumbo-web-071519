581c9f2f6b5efd0f0def7d7136749bedaea33b2c
jason <jasonedlewis@gmail.com>
2019-09-17 01:07:27 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,38 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { 
+            secondsLeft: this.props.initialCount
+        }
+        }
+        countDown (){
+        const newtime = this.state.secondsLeft -1 
+        this.setState({
+            secondsLeft: newtime
+        })
+        return this.state.secondsLeft
+        }
+        message (){
+            countDown()
+            if(this.state.secondsLeft !== 0){
+                return `${this.state.secondsLeft} seconds left before I go boom!`
+            }
+            else{
+                return 'Boom!!'
+        }
+    }
+            const ticToc = (){ setInterval(function(){
+                return message()
+
+            },1000)
+        }
+        
+    render(){
+        return ( 
+            
+        )
+    }
+}
